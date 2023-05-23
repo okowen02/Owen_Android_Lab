828f3588afce6b7385fcbae81e3ab1f93ecb073a
Owen Austin <okowen02@gmail.com>
2023-05-23 16:39:37 -0400

    Commit 10, Lab 2 Part 2, before ViewBinding

--- app/src/main/java/algonquin/cst2335/aust0076/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/aust0076/MainActivity.java
@@ -3,6 +3,10 @@ package algonquin.cst2335.aust0076;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView mytext = findViewById(R.id.textview);
+        Button mybutton = findViewById(R.id.mybutton);
+        EditText myedit = findViewById(R.id.myedittext);
+
+        mybutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String editString = myedit.getText().toString();
+                mytext.setText("Your edit text has: " + editString);
+            }
+        });
     }
 }
