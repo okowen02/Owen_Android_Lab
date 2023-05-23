b7330054263383952977a7dd0691903c2d245241
Owen Austin <okowen02@gmail.com>
2023-05-23 17:06:05 -0400

    Commit 11, Beginning of Lab 2 Part 4

--- app/src/main/java/algonquin/cst2335/aust0076/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/aust0076/MainActivity.java
@@ -8,22 +8,24 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import algonquin.cst2335.aust0076.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding variableBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(variableBinding.getRoot());
 
-        TextView mytext = findViewById(R.id.textview);
-        Button mybutton = findViewById(R.id.mybutton);
-        EditText myedit = findViewById(R.id.myedittext);
+        TextView mytext = variableBinding.textview;
+        Button mybutton = variableBinding.mybutton;
+        EditText myedit = variableBinding.myedittext;
 
-        mybutton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+        mybutton.setOnClickListener( vw -> {
                 String editString = myedit.getText().toString();
                 mytext.setText("Your edit text has: " + editString);
-            }
-        });
+            });
     }
 }
